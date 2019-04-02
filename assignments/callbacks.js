16b4197da6bcb237e969a61e8a1ea155e3c8fbcf
Carlil Jean <jaedagk@live.com>
2019-04-02 14:38:41 -0700

    Final.

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -24,25 +24,41 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
-
+getLength(value, function(length){
+  console.log(length);
+})
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length] - 1);
 }
+last(value, function(last){
+  console.log(last);
+})
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);
 }
-
+sumNums(value1, value2, function(additup){
+  console.log(additup);
+})
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y);
 }
-
+multiplyNums(value1, value2, function(times){
+  console.log(times);
+})
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
-  // Pass true to the callback if it is, otherwise pass false.
+  // Pass true to the callback if it is, otherwise pass false.}
+  return cb(item, list);
 }
-
+contains(item, list, function(value1, value2){
+    return value2.includes(value1);
+})
 /* STRETCH PROBLEM */
 
 function removeDuplicates(array, cb) {
--- assignments/closure.js
+++ assignments/closure.js
@@ -1,10 +1,17 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
-
+const make = 'Ducati';
+function drive() { 
+  const model = '7800';
+  alert(`The name of the bike is ${make} ${model}`);
+}
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  function(value){
+    value++;
+    return value;
+  }
 };
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
@@ -17,4 +24,5 @@ const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+    
 };
